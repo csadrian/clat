0615acd3468dc718f4a125407818639a296c2ed0
bbeatrix <bbea.smile@gmail.com>
2020-04-28 22:46:58 +0200

    Update script for feature extraction.

--- extract_features.py
+++ extract_features.py
@@ -7,15 +7,14 @@ from tensorflow.keras.models import Model
 
 tf.enable_eager_execution()
 
-MODEL_PATH = 'pretrained_models/cifar100vgg.h5'
-DATA_DIR = None
-DATASET_NAME = 'cifar100'
-NUM_CLASSES = 100
+MODEL_PATH = 'pretrained_models/cifar10vgg.h5'
+NUM_CLASSES = 10
 IMAGE_SHAPE = (32, 32, 3)
-OUT_LAYER_INDICES = [51]
+OUT_LAYER_INDICES = [44, 45, 46]
 BATCH_SIZE = 8
-SPLIT = 'train'
-
+DATA_DIR = None
+DATASET_NAME = 'cifar10'
+SPLIT = 'test'
 
 def preprocess(model_name, ds):
     print('Preprocessing images.')
@@ -170,18 +169,19 @@ def get_model(image_shape, num_classes, out_layer_idx, model_path=None):
                       input_shape=image_shape)
 
     for idx, layer in enumerate(model.layers):
-        print(idx, '. ', layer.name)
+        template = '{}. layer   name: {}   output shape: {}'
+        print(template.format(idx, layer.name, layer.output.shape))
 
     model.trainable = False
     out_layers = [model.layers[i] for i in out_layer_idx]
-    #out = [tf.reduce_max(item.output, axis=(1, 2)) for item in out_layers]
-    out = [item.output for item in out_layers]
+    out = [tf.reduce_max(item.output, axis=(1, 2)) for item in out_layers]
+    #out = [item.output for item in out_layers]
 
     new_model = Model(inputs=model.inputs, outputs=out)
     return new_model, out_layers
 
 
-def extract_featuremaps(ds_split, model_path, ds, model, out_layers, batch_size):
+def extract_featuremaps(ds_name, ds_split, model_path, ds, model, out_layers, batch_size):
     model_name = model_path.split('/')[-1]
     ds = preprocess(model_name, ds)
 
@@ -189,7 +189,7 @@ def extract_featuremaps(ds_split, model_path, ds, model, out_layers, batch_size)
     outputs = [[] for i in range(len(out_layers))]
     class_labels = []
 
-    if 'cifar100' in model_path:
+    if ds_name == 'cifar100':
         superclass_labels = []
     else:
         superclass_labels = None
@@ -217,18 +217,21 @@ def extract_featuremaps(ds_split, model_path, ds, model, out_layers, batch_size)
         print('Final array shape: ', output.shape)
         print('Saving {} layer outputs.'.format(out_layers[i].name))
         if superclass_labels is not None:
-            np.savez('{}_{}_{}_features'.format(ds_split, model_name.split('.')[0], out_layers[i].name),
+            np.savez('{}_{}_features_from_{}_{}'.format(ds_name, ds_split, model_name.split('.')[0], out_layers[i].name),
                      X=output,
                      y=target,
                      superclass=superclass_target)
         else:
-            np.savez('{}_{}_{}_features'.format(ds_split, model_name.split('.')[0], out_layers[i].name),
+            np.savez('{}_{}_features_from_{}_{}'.format(ds_name, ds_split, model_name.split('.')[0], out_layers[i].name),
                      X=output,
                      y=target)
 
 
 if __name__ == '__main__':
-    ds = get_dataset(DATASET_NAME, DATA_DIR, SPLIT)
     model, out_layers = get_model(IMAGE_SHAPE, NUM_CLASSES, OUT_LAYER_INDICES, MODEL_PATH)
-    extract_featuremaps(SPLIT, MODEL_PATH, ds, model, out_layers, BATCH_SIZE)
+
+    for DATASET_NAME in ['cifar10', 'cifar100']:
+        for SPLIT in ['train', 'test']:
+            ds = get_dataset(DATASET_NAME, DATA_DIR, SPLIT)
+            extract_featuremaps(DATASET_NAME, SPLIT, MODEL_PATH, ds, model, out_layers, BATCH_SIZE)
     print('fin')
