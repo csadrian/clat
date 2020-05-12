e339ce8cc7f1b2c7282e2ad4ea1d6bee680b514f
bbeatrix <bbea.smile@gmail.com>
2020-05-12 20:48:31 +0200

    Great update

--- feature_classifier.py
+++ feature_classifier.py
@@ -251,6 +251,18 @@ def build_model_meta_attention(num_classes, v_size=20, q_proj_trainable=True, k_
 
     return model, model_scores
 
+
+def build_model_pretrainedvgg16(num_classes, input_shape=(32,32,3)):
+    base_model = tf.keras.applications.VGG16(input_shape=input_shape, include_top=False, weights='imagenet')
+    base_model.trainable = False
+    x = base_model.output
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    logits = Dense(num_classes)(x)
+    predicted_probs = Activation('softmax')(logits)
+    model = Model(inputs=base_model.input, outputs=predicted_probs)
+    return model, None
+
+
 @gin.configurable
 def build_model(num_classes, model_name=gin.REQUIRED):
     if model_name == 'simple_ff':
@@ -259,12 +271,15 @@ def build_model(num_classes, model_name=gin.REQUIRED):
         return build_model_meta_attention(num_classes=num_classes)
     elif model_name == 'cifar100vgg_with_ma':
         return build_model_cifar100vgg_with_ma(num_classes=num_classes)
+    elif model_name == 'pretrainedvgg16':
+        return build_model_pretrainedvgg16(num_classes=num_classes)
     else:
         raise Exception('Model not implemented: ' + str(model_name))
 
 
+
 @gin.configurable
-def train_model(batch_size=64, epochs_per_class=20, num_splits=None):
+def train_model(batch_size=64, epochs_per_split=20, num_splits=None, step_with_generated=False):
 
     data = load_data(batch_size=batch_size)
     x_train, y_train, x_test, y_test = data.get_tuple()
@@ -291,6 +306,7 @@ def train_model(batch_size=64, epochs_per_class=20, num_splits=None):
 
     print('Train & test array shapes: ', x_train.shape, y_train.shape, x_test.shape, y_test.shape)
     train_datasets = []
+    labels_in_splits = []
 
     if num_splits > 1:
         assert  num_labels % num_splits == 0, (
@@ -306,8 +322,10 @@ def train_model(batch_size=64, epochs_per_class=20, num_splits=None):
             x_train_filtered = x_train[filter_mask]
             y_train_filtered = y_train[filter_mask]
             train_datasets.append((x_train_filtered, y_train_filtered))
+            labels_in_splits.append(concurrent_labels)
     else:
         train_datasets = [(x_train, y_train)]
+        labels_in_splits = list(range(num_labels))
 
     assert len(train_datasets) == num_splits, (
         'Number of training datasets must be equal to number of splits.')
@@ -315,16 +333,68 @@ def train_model(batch_size=64, epochs_per_class=20, num_splits=None):
     model, model_scores = build_model(num_classes = num_classes)
     model.summary()
 
-    reduce_lr = callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2,
-                                            patience=5, min_lr=0.00001)
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
 
-    model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
-                  optimizer=optimizers.SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True),
-                  metrics=['accuracy'])
-                  #callbacks=[reduce_lr])
+    optimizer = tf.keras.optimizers.SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
+
+    train_loss = tf.keras.metrics.Mean(name='train_loss')
+    train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+
+    test_loss = tf.keras.metrics.Mean(name='test_loss')
+    test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+
+    @tf.function
+    def train_step(images, labels):
+        with tf.GradientTape() as tape:
+            predictions = model(images, training=True)
+            loss = loss_object(labels, predictions)
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+        train_loss(loss)
+        train_accuracy(labels, predictions)
+
+
+    @tf.function
+    def test_step(images, labels):
+        predictions = model(images, training=False)
+        t_loss = loss_object(labels, predictions)
+
+        test_loss(t_loss)
+        test_accuracy(labels, predictions)
+
+
+    def generate_inputs(labels, batch_size, model, loss_object):
+        label_batch = np.random.choice(labels, batch_size, p=None)
+        shape = model.layers[0].input_shape[0][1:]
+        image = 128 * np.ones(shape, dtype=np.uint8)
+        image = tf.cast(image, tf.float32)
+        gray_img = image / 255.0
+        gray_img_batch = np.repeat(gray_img[np.newaxis,...], batch_size, axis=0)
+
+        for _ in range(10):
+            gray_imgs_tensor = tf.convert_to_tensor(gray_img_batch)
+
+            with tf.GradientTape() as tape:
+                tape.watch(gray_imgs_tensor)
+                predicted_probs = model(gray_imgs_tensor)
+                true_labels = tf.reshape(label_batch, (batch_size, 1))
+                loss = loss_object(true_labels, predicted_probs)
+
+            gradients = tape.gradient(loss, gray_imgs_tensor)
+            gray_img_batch = gray_img_batch + gradients
+
+        generated_input_batch = gray_img_batch + gradients
+
+        generated_ds = tf.data.Dataset.from_tensor_slices((generated_input_batch, label_batch)).batch(batch_size)
+        return generated_ds
+
+
 
     print('# Fit model on training data')
     hist_data = {'acc': [], 'loss': [], 'val_acc': [], 'val_loss': []}
+
+
     for i in range(num_splits):
         print('\nFIT ON {}. SPLIT\n'.format(i))
         x = train_datasets[i][0]
@@ -334,14 +404,43 @@ def train_model(batch_size=64, epochs_per_class=20, num_splits=None):
         x = x[:size]
         y = y[:size]
 
-        history = model.fit(x, y,
-                            batch_size=batch_size,
-                            epochs=epochs_per_class,
-                            validation_data=(x_test, y_test))
-        hist_data['acc'].extend(history.history['acc'])
-        hist_data['loss'].extend(history.history['loss'])
-        hist_data['val_acc'].extend(history.history['val_acc'])
-        hist_data['val_loss'].extend(history.history['val_loss'])
+        num_batches = size // batch_size
+        train_ds = tf.data.Dataset.from_tensor_slices((x, y)).batch(num_batches)
+        test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(x_test.shape[0]//batch_size)
+
+        for epoch in range(epochs_per_split):
+            train_loss.reset_states()
+            train_accuracy.reset_states()
+            test_loss.reset_states()
+            test_accuracy.reset_states()
+
+            for images, labels in train_ds:
+                train_step(images, labels)
+
+            for test_images, test_labels in test_ds:
+                test_step(test_images, test_labels)
+
+            template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
+            print(template.format(epoch + 1,
+                                  train_loss.result(),
+                                  train_accuracy.result() * 100,
+                                  test_loss.result(),
+                                  test_accuracy.result() * 100))
+            hist_data['acc'].append(train_accuracy.result())
+            hist_data['loss'].append(train_loss.result())
+            hist_data['val_acc'].append(test_accuracy.result())
+            hist_data['val_loss'].append(test_loss.result())
+
+            if step_with_generated:
+                # step with generated
+                train_loss.reset_states()
+                train_accuracy.reset_states()
+
+                seen_labels = list(np.array(labels_in_splits[:num_splits]).flat)
+
+                generated_ds = generate_inputs(seen_labels, batch_size, model, loss_object)
+                for images, labels in generated_ds:
+                    train_step(images, labels)
 
         if gin.query_parameter('build_model.model_name') in ['cifar100vgg_with_ma', 'meta_attention']:
             pred = model_scores.predict(x_test)
@@ -349,7 +448,6 @@ def train_model(batch_size=64, epochs_per_class=20, num_splits=None):
             for j in range(num_classes):
                 counts = np.bincount(np.argmax(pred[(y_test == j)], axis=-1), minlength=gin.query_parameter('build_model_meta_attention.v_size'))
                 all_counts.append(counts)
-                #print('counts: ', j, counts)
             all_counts = np.array(all_counts)
             heatmap = sns.heatmap(all_counts)
             buffer = io.StringIO()
@@ -361,8 +459,16 @@ def train_model(batch_size=64, epochs_per_class=20, num_splits=None):
             print('sum counts per v_size: ', np.sum(all_counts, axis=0))
 
     print('\n# Evaluate on test data')
-    results = model.evaluate(x_test, y_test, batch_size=batch_size)
-    print('Test loss, test acc: ', results)
+    test_loss.reset_states()
+    test_accuracy.reset_states()
+    test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(x_test.shape[0]//batch_size)
+
+    for test_images, test_labels in test_ds:
+        test_step(test_images, test_labels)
+
+    template = 'Test Loss: {}, Test Accuracy: {}'
+    print(template.format(test_loss.result(),
+                          test_accuracy.result() * 100))
     return hist_data
 
 
@@ -375,8 +481,6 @@ def main(argv):
 
         print(gin.operative_config_str())
         exp = neptune.create_experiment(params={}, name="exp")
-        #for tag in opts['tags'].split(','):
-        #  neptune.append_tag(tag)
     else:
         neptune.init('shared/onboarding', api_token='ANONYMOUS', backend=neptune.OfflineBackend())
 
@@ -384,6 +488,7 @@ def main(argv):
     plot_history(history, FLAGS.gin_file[0].split('/')[1].split('.')[0])
     neptune.stop()
     print('fin')
+
 
 if __name__ == '__main__':
     flags.DEFINE_multi_string('gin_file', None, 'List of paths to the config files.')
